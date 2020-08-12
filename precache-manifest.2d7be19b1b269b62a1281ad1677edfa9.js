6f136527fe9ffdd51384e9f52f4a3ffcb882c573
robbart-cy8 <64191721+robbart-cy8@users.noreply.github.com>
2020-08-12 16:04:34 +0530

    Updates

---
+++ precache-manifest.2d7be19b1b269b62a1281ad1677edfa9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17b3aa08c1a39f316ec6d6ae6d86513",
+    "url": "/smartigo-app/index.html"
+  },
+  {
+    "revision": "0f3fb2e7698206e462a8",
+    "url": "/smartigo-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1add16928218c4a37408",
+    "url": "/smartigo-app/static/css/main.264af3fe.chunk.css"
+  },
+  {
+    "revision": "0f3fb2e7698206e462a8",
+    "url": "/smartigo-app/static/js/2.92cd97c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartigo-app/static/js/2.92cd97c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1add16928218c4a37408",
+    "url": "/smartigo-app/static/js/main.40b07b1d.chunk.js"
+  },
+  {
+    "revision": "b96240a17917205bb503",
+    "url": "/smartigo-app/static/js/runtime-main.58c1db5a.js"
+  },
+  {
+    "revision": "0b7e5cc5b5519ab63c08373b542691d1",
+    "url": "/smartigo-app/static/media/icong.0b7e5cc5.png"
+  }
+]);
